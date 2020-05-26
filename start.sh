581cf156511d9370738acaf23dd7ac2922b955f4
maniasso <43616714+maniasso@users.noreply.github.com>
2020-05-26 14:59:07 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-docker run  -itd --name bind9 -p 53:53 -p 53:53/udp -v /root/docker/bind/bind:/bind  maniasso/bind
+docker run  -itd --name DNS --net=dns --ip 10.0.0.8 -p 53:53 -p 53:53/udp -v /root/docker/bind/conf:/bind -v  /root/docker/bind/conf/resolv.conf:/etc/resolv.conf maniasso/bind
